dd6a6582d8d243cb9a9d2fa61c93eee1bdec73cb
Sawai <c0a210628c@edu.teu.ac.jp>
2022-06-14 16:05:18 +0900

    サザエさんクイズ

---
+++ ex01/quiz.py
@@ -0,0 +1,26 @@
+import random
+
+def main():
+    seikai = shutudai()
+    kaito(seikai)
+
+def shutudai():
+    quiz = [{"q":"サザエの旦那の名前は？", "a":["マスオ", "ますお"]},
+            {"q":"カツオの妹の名前は？", "a":["ワカメ", "わかめ"]},
+            {"q":"タラオはカツオから見てどんな関係？", "a":["甥", "おい", "甥っ子", "おいっこ"]}]
+    print("問題：")
+    num = random.randint(0,2)
+    print(quiz[num]["q"])
+    return quiz[num]["a"]
+
+
+def kaito(seikai):
+    ans = input("答えるんだ：")
+    if ans in seikai:
+        print("正解！")
+    
+    else:
+        print("出直してこい")
+
+if __name__ == "__main__":
+    main()
