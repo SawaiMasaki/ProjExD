47f0e1a376d4bef3c91ba55336ec518fb1770a80
Sawai <c0a210628c@edu.teu.ac.jp>
2022-06-28 15:45:39 +0900

    練習１０まで

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,38 +1,50 @@
 import tkinter
+import maze_maker
 
-global key
 
 def key_down(event):
+    global key
     key = event.keysym
+    main_proc()
 
-def key_up():
+def key_up(event):
+    global key
     key = ""
 
 def main_proc():
-    if key == "Up":
-        cy -= 20
-    elif key == "Down":
-        cy += 20
-    elif key == "Left":
-        cx -= 20
-    elif key == "Right":
-        cx += 20
-    canvas.coords("tori", cx, cy)        
+    global cx, cy, mx, my
+
+    if key == "Up" and maze[my-1][mx]:
+        my -= 1
+    if key == "Down" and maze[my+1][mx]:
+        my += 1
+    if key == "Left" and maze[my][mx+1]:
+        mx -= 1
+    if key == "Right" and maze[my][mx-1]:
+        mx += 1
+
+    cx, cy = mx*100+50, mx*100+50
+    canvas.coords("tori", cx, cy)
+    root.after(100, main_proc)        
 
 if __name__ == "__main__":
-    global cx, cy
     root = tkinter.Tk()
     root.title("迷えるこうかとん")
     root.geometry("1500x900")
+
     key = ""
 
     canvas = tkinter.Canvas(root, width=1500, height=900, bg="black")
-    canvas.place(x=0, y=0)
+    canvas.pack()
 
-    
+    maze = maze_maker.make_maze(15, 9)
+    maze_maker.show_maze(canvas, maze)
 
-    tori = tkinter.PhotoImage(file="fig/5.jpg")
-    cx, cy = 300, 400
+    tori = tkinter.PhotoImage(file="fig/9.png")
+
+    mx, my = 1, 1
+    cx, cy = mx*100+50, mx*100+50
+
     canvas.create_image(cx, cy, image=tori, tag="tori")
 
     root.bind("<KeyPress>", key_down)
