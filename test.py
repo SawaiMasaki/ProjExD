cadb2ed19479bb6d2cf45f060e35864e55c8144d
Sawai <c0a210628c@edu.teu.ac.jp>
2022-07-05 17:24:40 +0900

    直しました #19

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("hello world")
+import tkinter
+
+def count_up():
+    global tmr
+    tmr = tmr + 1
+    label["text"] = tmr
+    root.after(1000, count_up)
+    
+if  __name__ == "__main__":
+    root = tkinter.Tk()
+    label = tkinter.Label(root,
+            font=("Times New Roman", 80)
+            )
+    label.pack()
+    tmr = 0
+    root.after(1000, count_up)
+    root.mainloop()
